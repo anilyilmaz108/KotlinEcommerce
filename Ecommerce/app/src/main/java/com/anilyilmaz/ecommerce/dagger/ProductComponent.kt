0497cf25bfb85612172dcb9206b73d844a4dcba1
anilyilmaz108 <anilyilmaz108@gmail.com>
2022-11-26 03:51:33 +0300

    EcommerceApi

--- Ecommerce/app/src/main/java/com/anilyilmaz/ecommerce/dagger/ProductComponent.kt
+++
@@ -1,9 +0,0 @@
-package com.anilyilmaz.ecommerce.dagger
-
-import com.anilyilmaz.ecommerce.activities.DetailsActivity
-import dagger.Component
-
-@Component(modules = [ProductModule::class])
-interface ProductComponent {
-    fun inject(activity: DetailsActivity)
-}
